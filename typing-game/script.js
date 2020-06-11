2dd3d19b42c2774899254be65d6a8341f0a1c5d9
dorsamoha <dorsamoha@gmail.com>
2020-06-11 20:33:37 +0300

    added random-words api and welcome screen

--- typing-game/script.js
+++ typing-game/script.js
@@ -7,32 +7,15 @@ const settingsBtn = document.getElementById('settings-btn');
 const settings = document.getElementById('settings');
 const settingsForm = document.getElementById('settings-form');
 const difficultySelect = document.getElementById('difficulty');
+const starGameBtn = document.getElementById('start-btn');
+const startGameEl = document.getElementById('startgame-container')
 
-// List of words for game
-const words = [
-    'sigh',
-    'tense',
-    'airplane',
-    'ball',
-    'pies',
-    'juice',
-    'warlike',
-    'bad',
-    'north',
-    'dependent',
-    'steer',
-    'silver',
-    'highfalutin',
-    'superficial',
-    'quince',
-    'eight',
-    'feeble',
-    'admit',
-    'drag',
-    'loving'
-    ];
 
-// Init word
+//Init game - start button pressed
+starGameBtn.addEventListener('click', e => {
+    startGameEl.style.display = 'none';
+
+const generateRandomWord = require("random-words");
 let randomWord;
 
 //Init score;
@@ -56,88 +39,113 @@ difficultySelect.value =
 //Focus on text on start
 text.focus();
 
-
 //Start counting down
 const timeInterval = setInterval(updateTime, 1000);
 
 //Update score function
 
-function updateScore(){
-    score++
-    scoreEl.innerHTML = score;
-};
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
 
 //Update Time
 
-function updateTime(){
-    time--;
-    timeEl.innerHTML = time + 's';
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
 
-    if(time === 0) {
-        clearInterval(timeInterval);
+  if (time === 0) {
+    clearInterval(timeInterval);
 
-        //end game
-        gameOver();
-    }
+    //end game
+    gameOver();
+  }
 }
 
 //Get a random word
 function getRandomWord() {
-    return words[Math.floor(Math.random() * words.length)]
+  randomWord = generateRandomWord();
+  console.log(randomWord);
 }
 
 //Add word to DOM
 
 function addWordToDOM() {
-    randomWord = getRandomWord();
+  getRandomWord();
 
-    word.innerHTML = randomWord;
+  word.innerHTML = randomWord;
 }
 
 //Game over show end screen
-function gameOver(){
-    endgameEl.innerHTML = `
+function gameOver() {
+  endgameEl.innerHTML = `
         <h1>Time ran out</h1>
         <p>Your final score is ${score}</p>
-        <button onclick="location.reload()">Play again</button>
+        <button id="reset">Play again</button>
     `;
 
-    endgameEl.style.display = 'flex';
+  endgameEl.style.display = "flex";
+  const resetBtn = document.getElementById("reset");
+  resetBtn.addEventListener('click', e => {
+      resetGame()
+  })
 }
 
-addWordToDOM()
+//Reset game function
+function resetGame() {
+    //Remove end game container
+    endgameEl.innerHTML = '';
+    endgameEl.style.display = 'none'
+   //Show start container
+   startGameEl.style.display = 'flex';
+
+   //reset time and score
+   scoreEl.innerHTML = '0'
+    time = 11
+   updateTime()
+
+}
+
+addWordToDOM();
 
 //Event listener
 
 //typing listener
-text.addEventListener('input', e => {
-    const insertedText = e.target.value;
-    // console.log(insertedText)
-    if(insertedText === randomWord) {
-        addWordToDOM()
-        updateScore()
-        //Clear 
-        e.target.value = '';
+text.addEventListener("input", (e) => {
+  const insertedText = e.target.value;
+  // console.log(insertedText)
+  if (insertedText === randomWord) {
+    addWordToDOM();
+    updateScore();
+    //Clear
+    e.target.value = "";
 
-        if(difficulty === 'hard'){
-            time += 2
-        } else if (difficulty === 'medium') {
-            time += 3
-        } else {
-            time +=5
-        }
-        updateTime()
-    };
+    if (difficulty === "hard") {
+      time += 2;
+    } else if (difficulty === "medium") {
+      time += 3;
+    } else {
+      time += 5;
+    }
+    updateTime();
+  }
 });
 
 //settings btn clicked
 
-settingsBtn.addEventListener('click', () => {
-    settings.classList.toggle('hide');
-})
+settingsBtn.addEventListener("click", () => {
+  settings.classList.toggle("hide");
+});
 
 //settings select
-settingsForm.addEventListener('change', e => {
-    difficulty = e.target.value;
-    localStorage.setItem("difficulty", difficulty);
-})
+settingsForm.addEventListener("change", (e) => {
+  difficulty = e.target.value;
+  localStorage.setItem("difficulty", difficulty);
+});
+
+});
+
+
+
+
