1bab3be048323f3b2a10cd451c218b7c71ef4e2c
dorsamoha <dorsamoha@gmail.com>
2020-06-30 16:22:05 +0300

    contact list

--- contacts-list/app.js
+++ contacts-list/app.js
@@ -2,6 +2,7 @@ const userInput = document.getElementById('filter-input');
 const names = [...document.querySelectorAll('.collection-item')];
 const headers = [...document.querySelectorAll('.collection-header')];
 const newContactBtn = document.getElementById('contact-form');
+const btn = document.getElementById('new-contact-btn');
 
 //Add event listener
 userInput.addEventListener('keyup', filterName);
@@ -18,6 +19,7 @@ function filterName(){
           name.style.display = '';
        } else {
            name.style.display = 'none';
+
        }
     });
 }
@@ -34,16 +36,15 @@ function addNewContact() {
     `
     headers.forEach(letter => {
         if (newContact.charAt(0).toUpperCase() === letter.innerText) {
-            letter.insertAdjacentHTML("afterend", markup)
-        } else {
-            const newHeader = `
-                <li class="collection-header"><h5>${newContact.charAt(0).toUpperCase()}</h5></li>
-            `
-            document.getElementById('names').insertAdjacentHTML('beforeend', newHeader);
-            // headers.sort()
+            letter.insertAdjacentHTML("afterend", markup);
         }
-    })
+    });
+
     //clear search form
     userInput.value = '';
+    // newContactBtn.reset()
+
+    //Refresh UI
+
 }
 
